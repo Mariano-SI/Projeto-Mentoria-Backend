620f24733841686035ebe3fa1a926ff40989321f
Mariano Silva <mariano.silva@ufvjm.edu.br>
2023-11-03 13:12:11 -0300

    subindo

--- src/infra/server/rabbitmqConfig.ts
+++ src/infra/server/rabbitmqConfig.ts
@@ -1,13 +1,9 @@
-import 'reflect-metadata';
-import ContestRepository from "./adapters/driven/adapter.database/repository/contestRepository"
-import { UpdateContestInput } from "./core/domain/dtos/contests/updateContestInput"
-import {Sequelize} from 'sequelize'
-import { server } from "./infra/server/config";
-
-import VoteRepository from "./adapters/driven/adapter.database/repository/votesRepository";
-import { DatabaseContext } from "./adapters/driven/adapter.database/context/DatabaseContext";
-import EnvironmentVariables from "./infra/environment/EnvironmentVariables";
 import amqp from 'amqplib/callback_api' 
+class RabbitmqConfig{
+    constructor() {
+        
+    }
+
 
 amqp.connect('amqp://localhost:5672', function (err, conn) {
     conn.createChannel(function (err, ch) {
@@ -33,16 +29,4 @@ amqp.connect('amqp://localhost:5672', function (err, conn) {
         }, { noAck: true });
     });
 });
-//server;
-
-
-/*function app(){
-    const teste = new ContestRepository( new Sequelize("mentoria-db", "Mariano", "m-88443244",{
-        host: "mentoria-server.database.windows.net",
-        port: 1433,
-        dialect:"mssql"
-    }))
- 
-    teste.selectAllContests().then((result)=> console.log(result))
 }
-app()*/
